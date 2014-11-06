40a7d8ce1816160fb270df2b359d56c8c53485a0
Honeybunch <amt3824@g.rit.edu>
2014-11-05 19:20:17 -0500

    Attempt to fix static files not being found on Openshift

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -16,3 +16,10 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+from django.conf import settings
+
+if settings.DEBUG:
+    urlpatterns += patterns('', (r'^media\/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT}),)
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
